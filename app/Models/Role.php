5d73389baa6175939b01621776e744ecff0628f3
Andrearusso94 <andrea.r.4845@gmail.com>
2023-01-18 16:47:47 +0100

    Add hasmany +belongsto in model/role+collaborator

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use Collator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Role extends Model
 {
     use HasFactory;
+    public function Collaborator(): HasMany
+    {
+        return $this->hasMany(Collator::class);
+    }
 }
